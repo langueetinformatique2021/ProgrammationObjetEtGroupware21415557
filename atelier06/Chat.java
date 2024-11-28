94afe51b64d6b3f62b1c3851ff4c344c69cbc771
N <N@JiatianLiu>
2024-11-28 10:58:09 +0100

    Chat et testChat

---
+++ atelier06/Chat.java
@@ -0,0 +1,34 @@
+package atelier06;
+
+public class Chat extends implements Domesticable {
+	package atelier06;
+
+	public class Chat extends Felin implements Domesticable {
+
+		/**
+		 * le constructeur de Chat fait appel au constructeur de la sur-classe Felin
+		 */
+		public Chat(String type) { // les chats ont 4 pattes
+			super(type);
+		}
+
+		/**
+		 * Domestiquer un animal et lui donner un nom
+		 * 
+		 * @param nom nom de l'animal
+		 */
+		public void domestiquer(String nom) {
+		}
+
+		/**
+		 * Rend le nom de l'animal
+		 * 
+		 * @return nom de l'animal
+		 */
+		public String nom() {
+			return null;
+		}
+
+	}
+
+}
---
+++ atelier06/testChat.java
@@ -0,0 +1,8 @@
+package atelier06;
+
+public class testChat {
+	public static void main(String[] args) {
+	Felin c = new Felin("chat");
+	c.présente();
+	c.crie();
+}
